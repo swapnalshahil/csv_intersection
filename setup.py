904e5d2e6f4d7ad0f04ecbf22c5398c0130da2c3
Swapnal <swapnalsahil@gmail.com>
2021-08-12 20:31:40 +0530

    final release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as desc:
 
 setuptools.setup(
     name="csv_intersection",
-    version="0.0.1",
+    version="1.0.0",
     author="Swapnal Shahil",
     author_email="swapnalsahil@gmail.com",
     description="CSV intersection",
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['csv', 'python-csv'],
+    install_requires=['python-csv'],
     python_requires='>=3.6',
     project_urls={
         'Bug Reports': 'https://github.com/swapnalshahil/csv_intersection/issues',
